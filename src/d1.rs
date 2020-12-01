0cd4a4bc3c4a0fad65cffa7cd9d105f575b6cfda
Alexandru Croitor <placinta@gmail.com>
2020-12-01 21:58:56 +0100

    Day 1 part 2 in rust

--- src/d1.rs
+++ src/d1.rs
@@ -56,9 +56,55 @@ fn solve_p1() {
     }
 }
 
-// fn solve_p2() {
-    
-// }
+#[derive(Debug)]
+struct ThreeNums(i64, i64, i64);
+impl PartialEq for ThreeNums {
+    fn eq(&self, other: &Self) -> bool {
+        let mut v1 = vec![self.0, self.1, self.2];
+        let mut v2 = vec![other.0, other.1, other.2];
+        v1.sort_unstable();
+        v2.sort_unstable();
+        return v1 == v2;
+    }
+}
+
+fn find_three_numbers_sum(target_sum: i64, numbers: &[i64]) -> Option<ThreeNums> {
+    let mut number_set = HashSet::new();
+    for number in numbers.iter() {
+        number_set.insert(number);
+    }
+    for n1 in numbers.iter() {
+        for n2 in numbers.iter() {
+            let complement: i64 = target_sum - n1 - n2;
+            if number_set.contains(&complement) {
+                return Some(ThreeNums(*n1, *n2, complement));
+            }
+        }
+    }
+    None
+}
+
+fn get_three_numbers_product(nums: &Option<ThreeNums>) -> Option<i64> {
+    match nums {
+        Some(ThreeNums(n1, n2, n3)) => Some(n1 * n2 * n3),
+        None => None,
+    }
+}
+
+fn solve_p2() {
+    const TARGET_SUM:i64 = 2020;
+    let data = helpers::get_data_from_file("d1").unwrap();
+    let numbers = helpers::lines_to_longs(&data);
+
+    if let Some(ThreeNums(n1, n2, n3)) = find_three_numbers_sum(TARGET_SUM, &numbers) {
+        println!("The 3 numbers summed to {} are: {}, {}, {}", TARGET_SUM, n1, n2, n3);
+
+        let result = get_three_numbers_product(&Some(ThreeNums(n1, n2, n3))).unwrap();
+        println!("The 3 numbers multipled are: {} ", result);
+    } else {
+        println!("No numbers summed to {}.", TARGET_SUM);
+    }
+}
 
 #[test]
 fn test_p1() {
@@ -68,7 +114,13 @@ fn test_p1() {
     assert_eq!(get_two_numbers_sum_and_product(TARGET_SUM, &[500, 1520]).1, Some(760000));
 }
 
+#[test]
+fn test_p2() {
+    const TARGET_SUM:i64 = 2020;
+    assert_eq!(find_three_numbers_sum(TARGET_SUM, &[1721, 979, 366, 299, 675, 1456]), Some(ThreeNums(979, 366, 675)));
+}
+
 fn main() {
     solve_p1();
-    // solve_p2();
+    solve_p2();
 }
