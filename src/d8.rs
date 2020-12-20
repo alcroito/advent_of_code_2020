0c16a23ecc3c889f229b8189b47a6fd928fef4d2
Alexandru Croitor <placinta@gmail.com>
2020-12-20 22:13:42 +0200

    Day 8 in rust

---
+++ src/d8.rs
@@ -0,0 +1,234 @@
+use advent::helpers;
+use advent::helpers::nom::NomError;
+use anyhow::{Context, Result};
+use std::convert::TryFrom;
+
+#[allow(unused_imports)]
+use nom::{
+    branch::alt,
+    bytes::complete::{tag, take_while_m_n},
+    character::complete::{alphanumeric0, alphanumeric1, digit1, multispace0, multispace1, one_of},
+    combinator::{all_consuming, map, map_res, recognize},
+    error::context,
+    multi::{separated_list0, separated_list1},
+    sequence::{pair, preceded, separated_pair, terminated},
+    IResult,
+};
+
+#[derive(Debug, Clone, Copy)]
+enum Instr {
+    Nop(i32),
+    Acc(i32),
+    Jmp(i32),
+}
+type Instructions = Vec<Instr>;
+type AccumulatorType = i32;
+
+#[derive(Debug, Clone)]
+struct Computer {
+    ip: usize,
+    instructions: Instructions,
+    acc: AccumulatorType,
+}
+
+#[derive(Debug, PartialEq)]
+enum ReturnStatus {
+    Regular,
+    Loop,
+}
+
+type EvalResult = (ReturnStatus, AccumulatorType);
+
+type NomErrorExact<'a> = NomError<&'a str>;
+
+fn parse_argument(i: &str) -> IResult<&str, i32, NomErrorExact> {
+    map_res(
+        recognize(pair(alt((tag("+"), tag("-"))), digit1)),
+        |s: &str| s.parse::<i32>(),
+    )(i)
+}
+
+fn parse_instruction(i: &str) -> IResult<&str, Instr, NomErrorExact> {
+    let parse_nop = context(
+        "nop",
+        map(
+            separated_pair(
+                tag("nop"),
+                nom::character::complete::char(' '),
+                parse_argument,
+            ),
+            |(_, arg)| Instr::Nop(arg),
+        ),
+    );
+    let parse_acc = context(
+        "acc",
+        map(
+            separated_pair(
+                tag("acc"),
+                nom::character::complete::char(' '),
+                parse_argument,
+            ),
+            |(_, arg)| Instr::Acc(arg),
+        ),
+    );
+    let parse_jmp = context(
+        "jmp",
+        map(
+            separated_pair(
+                tag("jmp"),
+                nom::character::complete::char(' '),
+                parse_argument,
+            ),
+            |(_, arg)| Instr::Jmp(arg),
+        ),
+    );
+    alt((parse_acc, parse_jmp, parse_nop))(i)
+}
+
+fn parse_instructions(i: &str) -> IResult<&str, Instructions, NomErrorExact> {
+    let parse_instructions = separated_list0(multispace1, parse_instruction);
+    all_consuming(terminated(
+        preceded(multispace0, parse_instructions),
+        multispace0,
+    ))(i)
+}
+
+impl<'a> std::convert::TryFrom<&'a str> for Computer {
+    type Error = anyhow::Error;
+
+    fn try_from(s: &'a str) -> Result<Self, Self::Error> {
+        use nom::Finish;
+
+        Ok(Computer::from_instructions(
+            parse_instructions(s)
+                .finish()
+                .map_err(|e| e.into_anyhow(s))?
+                .1,
+        ))
+    }
+}
+
+impl Computer {
+    fn from_instructions(instructions: Instructions) -> Self {
+        Computer {
+            ip: 0,
+            instructions,
+            acc: 0,
+        }
+    }
+
+    fn eval_instruction(&mut self, i: Instr) -> usize {
+        match i {
+            Instr::Acc(ref arg) => {
+                self.acc += *arg;
+                self.ip + 1
+            }
+            Instr::Jmp(ref arg) => (self.ip as i32 + *arg) as usize,
+            Instr::Nop(_) => self.ip + 1,
+        }
+    }
+
+    fn evaluate_until_loop(&mut self) -> EvalResult {
+        let mut executed_set = std::collections::HashSet::<usize>::new();
+        let max_ip = self.instructions.len();
+        loop {
+            if self.ip >= max_ip {
+                return (ReturnStatus::Regular, self.acc);
+            }
+            if executed_set.contains(&self.ip) {
+                return (ReturnStatus::Loop, self.acc);
+            }
+            executed_set.insert(self.ip);
+            let instr = self.instructions[self.ip];
+            self.ip = self.eval_instruction(instr);
+        }
+    }
+
+    fn fix_loop_and_eval(&self) -> EvalResult {
+        self.instructions
+            .iter()
+            .enumerate()
+            .filter(|(_, val)| matches!(val, Instr::Jmp(_) | Instr::Nop(_)))
+            .find_map(|(i, _)| {
+                let mut new_c = self.clone();
+                {
+                    let instr = &mut new_c.instructions[i];
+                    if let Instr::Nop(x) = instr {
+                        *instr = Instr::Jmp(*x);
+                    } else if let Instr::Jmp(x) = instr {
+                        *instr = Instr::Nop(*x);
+                    }
+                }
+                let (status, acc) = new_c.evaluate_until_loop();
+                match status {
+                    ReturnStatus::Loop => None,
+                    ReturnStatus::Regular => Some((status, acc)),
+                }
+            })
+            .expect("Expected to find a fixed loop program")
+    }
+}
+
+fn solve_p1() -> Result<()> {
+    let data = helpers::get_data_from_file_res("d8").context("Coudn't read file contents.")?;
+    let mut c = Computer::try_from(data.as_str()).expect("Invalid computer program\n");
+    println!(
+        "Accumulator value before encountering loop is: {}",
+        c.evaluate_until_loop().1
+    );
+    Ok(())
+}
+
+fn solve_p2() -> Result<()> {
+    let data = helpers::get_data_from_file_res("d8").context("Coudn't read file contents.")?;
+    let c = Computer::try_from(data.as_str()).expect("Invalid computer program\n");
+    let (_, acc) = c.fix_loop_and_eval();
+    println!("Accumulator value after fixing loop is: {}", acc);
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    solve_p1().ok();
+    solve_p2()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_p1() {
+        let input = "
+        nop +0
+        acc +1
+        jmp +4
+        acc +3
+        jmp -3
+        acc -99
+        acc +1
+        jmp -4
+        acc +6
+        ";
+        let mut c = Computer::try_from(input).expect("Invalid computer program\n");
+        assert_eq!(c.evaluate_until_loop().1, 5);
+    }
+
+    #[test]
+    fn test_p2() {
+        let input = "
+        nop +0
+        acc +1
+        jmp +4
+        acc +3
+        jmp -3
+        acc -99
+        acc +1
+        jmp -4
+        acc +6
+        ";
+        let c = Computer::try_from(input).expect("Invalid computer program\n");
+        let (status, acc) = c.fix_loop_and_eval();
+        assert_eq!(status, ReturnStatus::Regular);
+        assert_eq!(acc, 8);
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -16,7 +16,7 @@ pub mod helpers {
 
     pub fn get_data_from_file_res(name: &str) -> std::io::Result<String> {
         let path = format!("data/{}.txt", name);
-        fs::read_to_string(path) 
+        fs::read_to_string(path)
     }
 
     pub fn lines_to_longs(contents: &str) -> Vec<i64> {
@@ -40,5 +40,97 @@ pub mod helpers {
         }
         ints
     }
-}
 
+    pub mod nom {
+        #[derive(Debug)]
+        pub struct NomError<I>(nom::error::VerboseError<I>);
+
+        impl<I> NomError<I> {
+            pub fn into_verbose_string(self, i: I) -> String
+            where
+                I: core::ops::Deref<Target = str>,
+            {
+                nom::error::convert_error(i, self.0)
+            }
+
+            pub fn into_anyhow(self, i: I) -> anyhow::Error
+            where
+                I: core::ops::Deref<Target = str>,
+            {
+                anyhow::anyhow!("{}", self.into_verbose_string(i))
+            }
+        }
+
+        impl<I> nom::error::ParseError<I> for NomError<I> {
+            fn from_error_kind(input: I, kind: nom::error::ErrorKind) -> Self {
+                Self(nom::error::VerboseError {
+                    errors: vec![(input, nom::error::VerboseErrorKind::Nom(kind))],
+                })
+            }
+
+            fn append(input: I, kind: nom::error::ErrorKind, mut other: Self) -> Self {
+                other
+                    .0
+                    .errors
+                    .push((input, nom::error::VerboseErrorKind::Nom(kind)));
+                other
+            }
+
+            fn from_char(input: I, c: char) -> Self {
+                Self(nom::error::VerboseError {
+                    errors: vec![(input, nom::error::VerboseErrorKind::Char(c))],
+                })
+            }
+        }
+
+        impl<I, E> nom::error::FromExternalError<I, E> for NomError<I>
+        where
+            E: std::fmt::Display + 'static,
+        {
+            fn from_external_error(input: I, _kind: nom::error::ErrorKind, e: E) -> Self
+            where
+                E: std::fmt::Display + 'static,
+            {
+                // WARNING: this leaks memory.
+                // There's no other way to convert a String to a &'static str.
+                // And unfortunately nom::error::VerboseErrorKind::Context doesn't take an owned String.
+                // The proper way would be to re-implement our own error kind and VerboserError that can store a String,
+                // but then we can't use nom::error::convert_error :(
+                // So we'd have to copy-paste that function as well.
+                let leaked_external_error = Box::leak(format!("{}", e).into_boxed_str());
+                Self(nom::error::VerboseError {
+                    errors: vec![(
+                        input,
+                        nom::error::VerboseErrorKind::Context(leaked_external_error),
+                    )],
+                })
+            }
+        }
+
+        impl<I> nom::error::ContextError<I> for NomError<I> {
+            fn add_context(input: I, ctx: &'static str, mut other: Self) -> Self {
+                other
+                    .0
+                    .errors
+                    .push((input, nom::error::VerboseErrorKind::Context(ctx)));
+                other
+            }
+        }
+
+        pub fn dbg_dmp<'a, F, O, E: core::fmt::Debug>(
+            mut f: F,
+            context: &'static str,
+        ) -> impl FnMut(&'a str) -> nom::IResult<&'a str, O, E>
+        where
+            F: FnMut(&'a str) -> nom::IResult<&'a str, O, E>,
+        {
+            move |i: &'a str| match f(i) {
+                Err(e) => {
+                    println!("{}: Error({:?}) at:\n{}", context, e, i);
+                    Err(e)
+                }
+                a => a,
+            }
+        }
+    }
+}
